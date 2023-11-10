4a856542424abdf75a4a81af4585196582d78279
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-10 21:22:07 +0300

    feat: design category component

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [CommonModule,RouterOutlet],
+  imports: [CommonModule,RouterModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
