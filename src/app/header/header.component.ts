64ed874c6400de62ae25808dda73cc8ca3480c48
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-10 14:42:32 +0300

    feat: design header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebookF,faTwitter,faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  faFacebookF=faFacebookF;
+  faTwitter=faTwitter;
+  faInstagram=faInstagram;
+  faSearch=faSearch;
 }
