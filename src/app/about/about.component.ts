e249ec6bd042f29a50c5d586c3d3a1320a4d1484
Sedanur Çevik <sdnrcvk@gmail.com>
2023-11-10 21:23:14 +0300

    feat: design about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,5 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  name: string = "Sedanur Çevik";
+  graduatedFrom: string = "bilgisayar mühendisliği";
+  studiedAt: string = "Karabük Üniversitesi";
+  learning: string = ".Net , Next.js, Laravel";
+  collaborationInterest: string = "React Projects";
+  seekingHelpWith: string = "Laravel and .Net";
+  website: string = "www.sedanurcevik.com";
+  mediumProfile: string = "https://medium.com/@sdnrcvk";
+  githubProfile:string="https://github.com/sdnrcvk";
+  linkedinProfile: string = "https://www.linkedin.com/in/sedanurcevik/";
+  email: string = "sdnrcvk@gmail.com";
 }
